f572e9a6f3f249673288e3cd415d37b4a1916abd
ytalo azeredo <ytaloazeredo@gmail.com>
2022-10-31 04:32:06 -0300

    refactor: main component

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { MyRoutes } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-    <App />  
+    <MyRoutes/>
     </ChakraProvider>    
   </React.StrictMode>
 )
